17b148fb944fbf0015b55084fd6cf24920a9a6c6
MohamedRagheb <modyahmed221@gmail.com>
2022-09-28 02:00:14 +0200

    2

---
+++ jsfiles/indexmachine.js
@@ -0,0 +1,9 @@
+let newbox = document.getElementById('box');
+newbox.style.display = "none"
+
+function plus() {
+    newbox.style.display = "inline-block"
+}
+function nonplus() {
+    newbox.style.display = "none"
+}
