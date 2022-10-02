26f1dee34e27f5612f0771d50a428d6035ecf9e8
MohamedRagheb <modyahmed221@gmail.com>
2022-10-02 18:38:24 +0200

    almostdone(5)

--- jsfiles/indexmachine.js
+++ jsfiles/indexmachine.js
@@ -1,12 +1,16 @@
 let newbox = document.getElementById('box');
 let overlay = document.getElementById('overlay');
 let noteform = document.getElementById('noteform');
+let noteconti = document.getElementById('notediv')
 let notes = document.getElementById('notes');
 let reminds = document.getElementById('reminds');
 let notename = document.getElementById('nametext')
 let notetitle = document.getElementById('titlletext')
 let note = document.getElementById('note')
-let notecobnt = ' <div class="note" >'+notename+' </div>'
+let reminderform = document.getElementById('reminderform')
+let remindertext = document.getElementById('remindertext')
+let remindercolor = document.getElementById('remindercolor')
+let reminders = document.getElementById('reminders')
 
 newbox.style.display = "none"
 
@@ -27,7 +31,7 @@ function newnote(){
 }
 function newreminder(){
     overlay.style.display = "block"
-    noteform.style.display = "block"
+    reminderform.style.display = "block"
 }
 function notefilter() {
     reminds.style.display = "none"
@@ -37,10 +41,20 @@ function cancel(){
     overlay.style.display = "none"
     noteform.style.display = "none"
     newbox.style.display = "none"
+    reminderform.style.display = "none"
 }
 function addnewnote() {
    notes.innerHTML += '<div class="note" > <div class="topsection"><h4><b>'+notename.value+'</b></h4><h6><b>'+notetitle.value+'</b></h6></div> <div class="dwonsection"> <h5><font color="black" >'+note.value+'</font></h5></div></div>'
    overlay.style.display = "none"
     noteform.style.display = "none"
     newbox.style.display = "none"
+}
+function reminderfilter() {
+    noteconti.style.display = "none"
+}
+function addnereminder(){
+   overlay.style.display = "none"
+    reminderform.style.display = "none"
+    newbox.style.display = "none"
+    reminders.innerHTML += '<li class="reminder" style="color:'+remindercolor.value+';"> <font color="black">'+remindertext.value+'</font></li>'
 }
